1f98b4b2748afe4112f0c181cbc39d6e46cc6cb1
hansonq <lcacbcac@gmail.com>
2017-09-23 16:12:29 +0200

    Initial commit

---
+++ APIweather/src/pl/hanosnq/models/Config.java
@@ -0,0 +1,11 @@
+package pl.hanosnq.models;
+
+/**
+ * Created by lukasz on 2017-09-23.
+ */
+public class Config {
+    public static final String APP_ID = "b1514c97f34a6c256f04b9868e18fa80";
+    public static final String APP_BASE_URL = "http://api.openweathermap.org/data/2.5/weather?q=";
+
+
+}
