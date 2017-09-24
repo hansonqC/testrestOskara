4aec910f9f7cb5eeb056c81a2c955fc3b8f0a8c7
hansonq <lcacbcac@gmail.com>
2017-09-24 11:14:59 +0200

    Initial commit

--- APIweather/src/main/java/pl/hansonq/models/WeatherInfo.java
+++ APIweather/src/main/java/pl/hansonq/models/WeatherInfo.java
@@ -6,7 +6,34 @@ package pl.hansonq.models;
 public class WeatherInfo {
     private double temp;
     private int pressure;
+    private int visibility;
+    private int humidity;
 
+
+    public WeatherInfo(double temp, int pressure, int visibility, int humidity) {
+        this.temp = temp;
+        this.pressure = pressure;
+        this.visibility = visibility;
+        this.humidity = humidity;
+    }
+
+    public int getVisibility() {
+        return visibility;
+    }
+
+    public void setVisibility(int visibility) {
+        this.visibility = visibility;
+
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(int humidity) {
+        this.humidity = humidity;
+
+    }
 
     public WeatherInfo(double temp, int pressure) {
         this.temp = temp;
